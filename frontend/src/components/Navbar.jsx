b25e925da08f893f5c865f98ff3ff83d639dec99
Chirag Soni <csoni0693@gmail.com>
2024-12-17 16:35:42 +0530

    receipe

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { UserData } from '../context/User';
 
 const Navbar = () => {
-    const {logout} = UserData();
-
+    const {logout,user} = UserData();
+    
   return (
     <div className="bg-blue-500 p-4 flex justify-between items-center">
       <h1 className="text-white text-xl">RECEIPE</h1>
       <div className="flex space-x-4">
-        <button className="text-white p-2 rounded-md bg-green-600">Admin</button>
+        <button className={`text-white p-2 rounded-md bg-green-600 ${user.role == 'admin'?"block":"hidden"}`}>Admin</button>
         <button onClick={logout}  className="text-white bg-red-600 p-2 rounded-md">Logout</button>
       </div>
     </div>
